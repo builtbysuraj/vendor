0dabf15b036c34bbfdd1f8479318747c547229e6
builtbysuraj <surajgupta3553@gmail.com>
2023-10-02 21:00:34 +0530

    feat: Add MUI table pagination

--- client/src/components/Vendors.jsx
+++ client/src/components/Vendors.jsx
@@ -6,9 +6,12 @@ import {
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material"
+import { useState } from "react"
 import { Link as RouterLink } from "react-router-dom"
 
 import { useDelete } from "../hooks/useDelete"
@@ -17,51 +20,64 @@ import { useGet } from "../hooks/useGet"
 export default function Vendors() {
   const { vendorData, fetchVendors, error } = useGet()
   const { handleDelete } = useDelete()
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+
+  const handleChangePage = (e, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (e) => {
+    setRowsPerPage(parseInt(e.target.value, 10))
+    setPage(0)
+  }
 
   const handleDeleteAndRefresh = async (id) => {
     await handleDelete(id)
     fetchVendors()
   }
 
-  const renderedData = vendorData?.map((ele) => (
-    <TableRow
-      sx={{
-        "& .MuiTableCell-root": {
-          fontSize: "1rem",
-        },
-      }}
-      key={ele._id}
-    >
-      <TableCell component="th" scope="row">
-        {ele.vendorName}
-      </TableCell>
-      <TableCell align="right">{ele.bankAccNum}</TableCell>
-      <TableCell align="right">{ele.bankName}</TableCell>
-      <TableCell align="right">
-        <RouterLink
-          style={{ textDecoration: "none", color: "blue" }}
-          to={`/edit/${ele._id}`}
-        >
-          Edit
-        </RouterLink>
-      </TableCell>
-      <TableCell align="right">
-        <RouterLink
-          to="#"
-          style={{ textDecoration: "none", color: "red" }}
-          onClick={() => {
-            if (
-              window.confirm("Are you sure you want to delete this vendor?")
-            ) {
-              handleDeleteAndRefresh(ele._id)
-            }
-          }}
-        >
-          Delete
-        </RouterLink>
-      </TableCell>
-    </TableRow>
-  ))
+  const renderedData = vendorData
+    ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    .map((ele) => (
+      <TableRow
+        sx={{
+          "& .MuiTableCell-root": {
+            fontSize: "1rem",
+          },
+        }}
+        key={ele._id}
+      >
+        <TableCell component="th" scope="row">
+          {ele.vendorName}
+        </TableCell>
+        <TableCell align="right">{ele.bankAccNum}</TableCell>
+        <TableCell align="right">{ele.bankName}</TableCell>
+        <TableCell align="right">
+          <RouterLink
+            style={{ textDecoration: "none", color: "blue" }}
+            to={`/edit/${ele._id}`}
+          >
+            Edit
+          </RouterLink>
+        </TableCell>
+        <TableCell align="right">
+          <RouterLink
+            to="#"
+            style={{ textDecoration: "none", color: "red" }}
+            onClick={() => {
+              if (
+                window.confirm("Are you sure you want to delete this vendor?")
+              ) {
+                handleDeleteAndRefresh(ele._id)
+              }
+            }}
+          >
+            Delete
+          </RouterLink>
+        </TableCell>
+      </TableRow>
+    ))
 
   return (
     <>
@@ -85,6 +101,18 @@ export default function Vendors() {
               </TableRow>
             </TableHead>
             <TableBody>{renderedData}</TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25]}
+                  count={vendorData.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
         </TableContainer>
       ) : (
--- client/src/hooks/useDelete.js
+++ client/src/hooks/useDelete.js
@@ -1,7 +1,6 @@
 export function useDelete() {
 
   const handleDelete = async (id) => {
-    console.log("Handle delete")
     await fetch(`http://localhost:3000/vendors/${id}`, {
       method: "DELETE",
     })
